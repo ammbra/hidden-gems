5a9bbb31feb5b773250f0cf834c36bb99eef33d3
ammbra <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>
2022-07-16 22:57:28 +0200

    updated README.adoc and small changes in configs

--- activity/src/main/java/org/acme/entertainment/RandomHobbyResource.java
+++ activity/src/main/java/org/acme/entertainment/RandomHobbyResource.java
@@ -4,6 +4,7 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.extension.annotations.WithSpan;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
@@ -43,12 +44,10 @@ public class RandomHobbyResource {
 
     @GET
     public Response getRandomHobby() throws InterruptedException {
-        Response response;
         RandomHobby hobby = service.getActivity();
         hobby.setCloudId(cloudId);
         hobby.setWorkerId(ID);
-        Thread.sleep((long)(Math.random() * 1000));
-        return   response = Response.status(Response.Status.OK).entity(hobby).build();
+        return (hobby.getPrice() < 0.1) ? timeout("busywork") : Response.status(Response.Status.OK).entity(hobby).build();
 
     }
 
@@ -87,19 +86,19 @@ public class RandomHobbyResource {
         // put the span into the current Context
         try (Scope scope = span.makeCurrent()) {
             LOGGER.debug(String.format("Thread interrupted on %s", cloudId));
-                Thread.sleep(2000);
-             span.setAttribute("timeout", "exec");
-             response = Response.status(Response.Status.GATEWAY_TIMEOUT)
-                        .entity(service.getActivityByType(type)).build();
-        } catch(Exception t){
-                span.setAttribute("alarm", "unexpected");
-                span.setStatus(StatusCode.ERROR, "Something wrong happened!");
-                span.recordException(t);
-                response = Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                        .build();
-            } finally {
-                span.end(); // Cannot set a span after this call
-            }
+            Thread.sleep(2000);
+            span.setAttribute("timeout", "exec");
+            response = Response.status(Response.Status.GATEWAY_TIMEOUT)
+                    .entity(service.getActivityByType(type)).build();
+        } catch (Exception t) {
+            span.setAttribute("alarm", "unexpected");
+            span.setStatus(StatusCode.ERROR, "Something wrong happened!");
+            span.recordException(t);
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .build();
+        } finally {
+            span.end(); // Cannot set a span after this call
+        }
         return response;
 
     }
