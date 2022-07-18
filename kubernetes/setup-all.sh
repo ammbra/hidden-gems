74473c5ff6d4b8fd85cfdd399d47176ea9cad802
ammbra <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>
2022-07-18 09:24:08 +0200

    updated README.adoc

--- kubernetes/setup-all.sh
+++ kubernetes/setup-all.sh
@@ -10,4 +10,10 @@ kubectl replace -f prometheus-deployment.yaml
 oc expose svc prometheus
 oc expose svc jaeger --port=16686 --generator="route/v1"
 
-curl -O "https://raw.githubusercontent.com/redhat-scholars/kube-native-java-apps/master/apps/kubefiles/{jaeger-server-configmap.yaml,prometheus-configmap.yaml,otel-configmap.yaml,jaeger-deployment.yaml,jaeger-service.yaml,otel-collector-deployment.yaml,prometheus-deployment.yaml}"
+kubectl apply -f activity-deployment.yaml
+kubectl apply -f activity-service.yaml
+kubectl apply -f hobby-deployment.yaml
+kubectl apply -f hobby-service.yaml
+
+
+#curl -O "https://raw.githubusercontent.com/redhat-scholars/kube-native-java-apps/master/apps/kubefiles/{jaeger-server-configmap.yaml,prometheus-configmap.yaml,otel-configmap.yaml,jaeger-deployment.yaml,jaeger-service.yaml,otel-collector-deployment.yaml,prometheus-deployment.yaml}"
